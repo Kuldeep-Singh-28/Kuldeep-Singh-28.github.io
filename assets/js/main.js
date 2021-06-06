01ab6b9b01a9b466b2af095e98f76059618b05f1
Kuldeep-Singh-28 <kuldeepsingh281999@gmail.com>
2021-06-06 22:04:27 +0530

    Add Portfolio Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,3 +250,135 @@
     $(".preloader").removeClass("active");
   });
 })(jQuery);
+
+/*--------------------------------
+     7.1. Image Gallery Popup
+     ----------------------------------*/
+function image_gallery() {
+  $(".gallery-popup-btn").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+}
+image_gallery();
+
+/*--------------------------------
+     7.2. Youtube - Vimeo - SoundCloud Popup
+     ----------------------------------*/
+function video_soundcloud() {
+  $(".magnific-youtube,.magnific-vimeo,.magnific-soundcloud").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 300,
+    preloader: true,
+    fixedContentPos: false,
+  });
+}
+video_soundcloud();
+
+/*--------------------------------
+     7.3. Porfolio Detail Popup
+     ----------------------------------*/
+function porfolio_detail_popup() {
+  $(".portfolio-detail-popup").magnificPopup({
+    type: "inline",
+    mainClass: "mfp-fade",
+    preloader: true,
+    gallery: {
+      enabled: true,
+    },
+  });
+}
+porfolio_detail_popup();
+
+/*--------------------------------
+     7.4. Blog Detail Popup
+     ----------------------------------*/
+function blog_detail_popup() {
+  $(".blog-detail-popup").magnificPopup({
+    type: "inline",
+    mainClass: "mfp-fade",
+    preloader: true,
+    gallery: {
+      enabled: true,
+    },
+  });
+}
+blog_detail_popup();
+
+/*--------------------------------
+     08. Form Validation
+     ----------------------------------*/
+window.addEventListener(
+  "load",
+  function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName("needs-validation");
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener(
+        "submit",
+        function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  },
+  false
+);
+
+/*--------------------------------
+     09. Menu Toggle
+     ----------------------------------*/
+$(".menu-toggle").on("click", function () {
+  $(".navigation-menu").addClass("toggled");
+  $(".navigation-menu").toggleClass("mobile-menu-hide");
+  $(".menu-toggle").toggleClass("open");
+});
+function mobileMenuHide() {
+  var windowWidth = $(window).width(),
+    siteHeader = $(".navigation-menu");
+  if (windowWidth < 992) {
+    siteHeader.addClass("mobile-menu-hide");
+    $(".menu-toggle").removeClass("open");
+    setTimeout(function () {
+      siteHeader.addClass("toggled");
+    }, 500);
+  } else {
+    siteHeader.removeClass("toggled");
+  }
+}
+$(".navigation-menu").on("click", "a:not(.sublink)", function (e) {
+  e.preventDefault();
+  mobileMenuHide();
+});
+
+/*--------------------------------
+     10. Back To Top Scroll Arrow
+     ----------------------------------*/
+function back_to_top() {
+  $(".back-to-top").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+    return false;
+  });
+}
+back_to_top();
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $(".back-to-top").fadeIn();
+  } else {
+    $(".back-to-top").fadeOut();
+  }
+});
