ec689e284f04d8d00c70db5ed369886875d33a2b
Kuldeep-Singh-28 <kuldeepsingh281999@gmail.com>
2021-06-07 20:51:02 +0530

    add mail sender

--- assets/js/main.js
+++ assets/js/main.js
@@ -437,7 +437,7 @@
     if (valid) {
       $.ajax({
         type: "POST",
-        url: "",
+        url: "mail.php",
         data: frm.serialize(),
         beforeSend: function () {
           $(".loading").show();
@@ -459,29 +459,29 @@
   });
 
   /* Disable Copy And Key  */
-  $(window).on("load", function () {
-    document.onkeydown = function (e) {
-      if (e.keyCode == 123) {
-        return false;
-      }
-      if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-        return false;
-      }
-      if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-        return false;
-      }
-      if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-        return false;
-      }
-      if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-        return false;
-      }
-    };
-    $("html").on("contextmenu", function () {
-      return false;
-    });
-    $("html").bind("cut copy", function (e) {
-      e.preventDefault();
-    });
-  });
+  // $(window).on("load", function () {
+  //   document.onkeydown = function (e) {
+  //     if (e.keyCode == 123) {
+  //       return false;
+  //     }
+  //     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+  //       return false;
+  //     }
+  //     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+  //       return false;
+  //     }
+  //     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+  //       return false;
+  //     }
+  //     if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+  //       return false;
+  //     }
+  //   };
+  //   $("html").on("contextmenu", function () {
+  //     return false;
+  //   });
+  //   $("html").bind("cut copy", function (e) {
+  //     e.preventDefault();
+  //   });
+  // });
 })(jQuery);
